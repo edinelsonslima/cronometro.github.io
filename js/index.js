79de83e2a587c358f78fd2615f610998b5cf8813
edinelson <edinelsonlima86@gmail.com>
2021-04-26 20:35:48 -0300

    Count

--- js/index.js
+++ js/index.js
@@ -1,47 +1,49 @@
+/*Chamando as variavéis nescessárias pelo DOM*/
 const hr = document.querySelector('#hour')
 const min = document.querySelector('#min')
 const sec = document.querySelector('#sec')
 const hun = document.querySelector('#hun')
 const ini = document.querySelector('#ini')
 const pau = document.querySelector('#pause')
+
+/*Inicializando as variavéis*/
 let cont=1, pauseButton = false
 let centesimas = 0;
 let segundos = 0;
 let minutos = 0;
 let horas = 0;
 
-
+/*Função para clique no botão iniciar*/
 ini.addEventListener('click',()=>{
-    //clearInterval(control)
-    control = setInterval(relogio, 10)
-    ini.disabled = true
-    pauseButton = true
-    console.log('Iniciando')
+    control = setInterval(relogio, 10) //Chama a função relogio com um delay de 10
+    ini.disabled = true //Desativa o botão iniciar para evitar bugs
+    pauseButton = true //Aciona a possibilidade de acionar o botão pause
 })
 
+/*Função para zerar relógio e variaveis*/
 function zerar(){
-    clearInterval(control)
-    ini.disabled = false
-    pauseButton = false
+    clearInterval(control) //Encerra o metodo setInterval
+    ini.disabled = false   //Ativa o botão Iniciar
+    pauseButton = false    //Desativa o botão pause para não da bug
     
+    //Limpando as variaveis do HTML
     pau.value = 'PAUSE'
     hun.innerHTML = " 00"
     sec.innerHTML = " 00"
     min.innerHTML = " 00"
     hr.innerHTML = " 00"
     
+    //Limpando as variaveis do JS
     centesimas = 0;
     segundos = 0;
     minutos = 0;
     horas = 0;
-    console.log('Zerado')
 }
 
-
-    
+/*Função botão pause/play*/
 pau.addEventListener('click',()=>{  
-    if(pauseButton){
-        cont ++
+    if(pauseButton){ //Se retornar TRUE pode acessar o botão pause
+        cont ++ // conta os cliks no botão pause
         if(cont % 2 == 0){
             pau.value = 'PLAY'
             clearInterval(control)
@@ -55,6 +57,7 @@ pau.addEventListener('click',()=>{
     }
 })
 
+/*Lógica do cronometro*/
 function relogio(){
     if (centesimas < 99) {
         centesimas++;
